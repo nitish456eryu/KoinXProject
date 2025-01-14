06d9612dda65b70e76f14323baa172a07eada36b
nitish456eryu <126476191+nitish456eryu@users.noreply.github.com>
2025-01-15 01:03:08 +0530

    add about coin info

--- src/components/Aboutbitcoin.jsx
+++ src/components/Aboutbitcoin.jsx
@@ -4,16 +4,30 @@ import '../components/headerdesign.css';
 const About = () => {
     return (
         <>
+        <div className='bitcoin_box'>
             <div className='bitcoin'>
                 About Bitcoin
             </div>
-            <h2>About Bitcoin</h2>
-
-            <h4>What is Bitcoin?</h4>
-            
-            <h4>Lorem ipsum dolor sit amet</h4>
-            <p></p>
-
+            <div className='bitcoin_header'>
+                What is Bitcoin?
+            </div>
+            <div className='bitcoin_description'>
+                Bitcoin's price today is US$16,951.82, with a 24-hour trading volume of $19.14 B. BTC is +0.36% in the last 24 hours. It is currently -7.70% from its 7-day all-time high of $18,366.66, and 3,40% from its 7-day all-time low of $16,394.75. BTC has a circulating supply of 19.24 M BTC and a max supply of 21 M BTC.
+            </div>
+            <hr />
+            <div className='bitcoin_header'>
+                Lorem ipsum dolor sit amet
+            </div>
+            <div style={{fontWeight:"500"}}>
+                Lorem ipsum dolor sit amet consectetur. Aliquam placerat sit lobortis tristique pharetra. Diam id et lectus una et tellus aliquam dictum at. Viverra diam suspendisse enim facilisi diam ut sed. Quam scelerisque fermentum sapien morbi sodales odio sed rhoncus. Ultricies urna volutpat pendisse enim facilisi diam ut sed. Quam scelerisque fermentum sapien morbi sodales odio sed rhoncus.
+            </div>
+            <div style={{fontWeight:"500"}}>
+                Diam praesent massa dapibus magna aliquam a dictumst volutpat. Egestas vitae pellentesque auctor amet. Nunc sagittis libero adipiscing cursus felis pellentesque interdum. Odio cursus phasellus velit in senectus enim dui. Turpis tristique placerat interdum sed volutpat. Id imperdiet magna eget eros donec cursus nunc. Mauris faucibus diam mi nunc praesent massa turpis a. Integer dignissim augue viverra nulla et quis lobortis phasellus. Integer pellentesque enim convallis ultricies at.
+            </div>
+            <div style={{fontWeight:"500"}}>
+                Fermentum hendrerit imperdiet nulla viverra faucibus. Sit aliquam massa vel convallis duis ac. Mi adipiscing semper scelerisque porttitor pulvinar nunc risus. Fermentum potenti iaculis lacinia congue ipsum fames amet dul. Purus ultrices tincidunt volutpat in eget. Ullamcorper dui
+            </div>
+        </div>
         </>
     )
 }
